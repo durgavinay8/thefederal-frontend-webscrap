e8cc93138098eaadf370665f2e9439d2e133104e
durgavinay8 <113960662+durgavinay8@users.noreply.github.com>
2023-08-21 13:50:41 +0530

    Add files via upload

---
+++ thefederal-clone/script-index.js
@@ -0,0 +1,25 @@
+let dataToBeRendered;
+
+//Fetching the data to be rendered when the html is loaded
+document.addEventListener("DOMContentLoaded", ()=>{
+   //fetch call
+});
+
+const renderAllData = async ()=>{
+
+}
+
+const renderHeader = async()=>{
+    const header = document.getElementById('header-tag');
+    //update logo
+    header.querySelector('the-federal-logo').setAttribute('src',);
+    
+}
+
+const renderMain = async()=>{
+
+}
+
+const renderFooter = async()=>{
+
+}
